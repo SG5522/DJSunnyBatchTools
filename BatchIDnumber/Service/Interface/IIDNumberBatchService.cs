aadc3fa6639474cf5cb7d60ee7235f7da586781d
SG552 <SG552@djimage.com.tw>
2025-09-22 17:48:41 +0800

    docs:補上註解。

--- BatchIDnumber/Service/Interface/IIDNumberBatchService.cs
+++ BatchIDnumber/Service/Interface/IIDNumberBatchService.cs
@@ -6,8 +6,18 @@ namespace BatchIDnumber.Service.Interface
 {
     public interface IIDNumberBatchService
     {
+        /// <summary>
+        /// 依照身份別取得Orders清單
+        /// </summary>
+        /// <param name="customerTypes"></param>
+        /// <returns></returns>
         Task<List<OrdersView>> GetOrders(List<string> customerTypes);
 
+        /// <summary>
+        /// 變更籌備處、聯名戶的統編
+        /// </summary>
+        /// <param name="ordersViews"></param>
+        /// <returns></returns>
         Task Process(List<OrdersView> ordersViews);
     }
 }
